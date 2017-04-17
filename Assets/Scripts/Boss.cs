316e0c0a25462ffc5708191e38ea4adeb01dc059
Stuart Aaron Millman <stu.millman15@gmail.com>
2017-04-17 14:29:01 +0100

    Boss Animations + Spell

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(SpellController))]
 public class Boss : Enemy {
 
+	private const int STATE_RITUAL = 0;
+	private const int STATE_ATTACK_IDLE = 1;
+	private const int STATE_ATTACK = 2;
+	private const int STATE_DEAD = 3;
+
 	private SpellController spellController;
+
+	private bool isPlaying_Attack;
 
 	private float defaultXScale;
 
@@ -13,13 +20,23 @@ public class Boss : Enemy {
 		spellController = GetComponent<SpellController>();
 
 		defaultXScale = transform.localScale.x;
+
+		ChangeState(STATE_RITUAL);
 	}
 
 	private void Update() {
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("fanngAttack"))
+			isPlaying_Attack = true;
+		else
+			isPlaying_Attack = false;
+
 		if(attackCooldownTimer > 0)
 			attackCooldownTimer -= Time.deltaTime;
 
-		if(seesPlayer) {
+		if(seesPlayer)
+			HandleFaceDir(character.transform.position);
+
+		if(seesPlayer && !isPlaying_Attack) {
 			float dist = Vector3.Distance(transform.position, character.transform.position);
 			if(dist <= attackDistance && attackCooldownTimer <= 0f) {
 				Attack(character);
@@ -32,8 +49,10 @@ public class Boss : Enemy {
 
 		if(!seesPlayer)
 			if(Vector3.Distance(transform.position, character.transform.position) <= visionDistance)
-				if(SeesPlayer())
+				if(SeesPlayer()) {
 					seesPlayer = true;
+					ChangeState(STATE_ATTACK_IDLE);
+				}
 	}
 
 	private bool SeesPlayer() {
@@ -52,24 +71,32 @@ public class Boss : Enemy {
 		return false;
 	}
 
-	protected override void Attack(Entity _e) {
-		Vector3 playerPos = character.transform.position + new Vector3(0, 1f, 0);
-		Vector3 dir = (playerPos - spellController.spellOrigin.position).normalized;
+	private void HandleFaceDir(Vector3 _target) {
+		Vector3 charDir = (transform.position - _target).normalized;
 
-		Vector3 charDir = (playerPos - transform.position).normalized;
 		Vector3 scale = transform.localScale;
 		if(charDir.x < 0)
 			scale.x = -defaultXScale;
 		else if(charDir.x > 0)
 			scale.x = defaultXScale;
 		transform.localScale = scale;
+	}
+
+	protected override void Attack(Entity _e) {
+		Vector3 playerPos = character.transform.position + new Vector3(0, 1f, 0);
+		Vector3 dir = (playerPos - spellController.spellOrigin.position).normalized;
 
 		spellController.UseActiveSpell(dir, _e);
+
+		ChangeState(STATE_ATTACK);
 	}
 
 	protected override void OnDie() {
 		LevelManager.AddScore(LevelManager.SCORE_PER_KILL);
-		Destroy(gameObject);
+		ChangeState(STATE_DEAD);
+
+		this.enabled = false;
+
 	}
 
 }
