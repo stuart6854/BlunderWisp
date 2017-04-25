2f5c3ab0b7e9f974b7b9f7f4ceb0f3d878dfa9ba
Stuart Aaron Millman <stu.millman15@gmail.com>
2017-04-25 19:23:11 +0100

    Anims + Audio

--- Assets/Scripts/PuzzleDoor.cs
+++ Assets/Scripts/PuzzleDoor.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class PuzzleDoor : PuzzleAction {
 
+	public AudioClip sound;
+
 	public override void OnPuzzleTriggered() {
+		if(sound != null)
+			GetComponent<AudioSource>().PlayOneShot(sound);
+
 		gameObject.SetActive(false);
 	}
 
